38659e7868fb7cdca90228c3dae96dda18a4cb80
Parkpoom Wisedsri <95712446+parkpoomdev@users.noreply.github.com>
2023-10-28 07:34:40 +0000

    feat: Update app.js and add new files
    Updated app.js file to use single quotes, changed database connection, added new routes, and added new files tasks.json and devcontainer.json.

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-// database connection
-const dbURI = 'mongodb+srv://bond007:qwerty12345@nodecluster.gmpfhu2.mongodb.net/NodeAuth1';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(err));
+// // database connection
+// const dbURI =
+//   "mongodb+srv://st123861:Viseszri1991Best@cluster0.krss1bo.mongodb.net/angularproject";
+// mongoose
+//   .connect(dbURI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//   })
+//   .then((result) => app.listen(3000))
+//   .catch((err) => console.log(err));
 
 // routes
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
